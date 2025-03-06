5b4ff8b52ae7bfd57603950c77d82e4f8ccaecef
karpov2713 <karpov2713@gmail.com>
2025-03-06 16:09:38 +0300

    18 Наследование классов

--- JAVA_VAR_TASK/src/Main.java
+++ JAVA_VAR_TASK/src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Figure geom[] = new Figure[4];
+        geom[0] = new Line();
+        geom[1] = new Triangle();
+        geom[2] = new Rectangle();
+        geom[3] = new Ellipse();
     }
+}
+
+class Geom{
+    int id;
+}
+
+class Figure extends Geom{
+    int width;
+    int color;
+
+    Figure() {
+        System.out.println("Конструктор Figure");
+    }
+}
+
+class Line extends Figure {
+    double x1, y1;
+    double x2, y2;
+
+    Line(){
+        System.out.println("Конструктор Line");
+    }
+}
+
+class Triangle extends Figure {
+    double x1, y1;
+    double x2, y2;
+    double x3, y3;
+}
+
+class Rectangle extends Figure {
+    double x1, y1;
+    double x2, y2;
+}
+
+class Ellipse extends Figure {
+    double x1, y1;
+    double x2, y2;
 }
