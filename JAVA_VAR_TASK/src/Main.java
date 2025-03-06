bf14362daf3c51bc64931ced993c8f6a5f091562
karpov2713 <karpov2713@gmail.com>
2025-03-06 17:21:52 +0300

    19 оператор instanceof

--- JAVA_VAR_TASK/src/Main.java
+++ JAVA_VAR_TASK/src/Main.java
@@ -1,7 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        Line line = new Line();
-        line.showId();
+        Object g1 = new Line();
+        Object g2 = new Triangle();
+        Object g3 = new Figure();
+
+        Line l1 = (Line) g1;
+        l1.showId();
+
+        Line l2 = null;
+        if (g2 instanceof Line) {
+            l2 = (Line) g2;
+        }
     }
 }
 
@@ -14,13 +23,13 @@ class Figure {
         System.out.println("Конструктор Figure()");
     }
 
-    Figure(int width, int color){
+    Figure(int width, int color) {
         this.width = width;
         this.color = color;
         System.out.println("Конструктор Figure(width, color)");
     }
 
-    void showId(){
+    void showId() {
         System.out.println("id = " + id);
     }
 }
@@ -30,12 +39,12 @@ class Line extends Figure {
     double x2, y2;
     int id = 2;
 
-    Line(){
-        super(0,0);
+    Line() {
+        super(0, 0);
         System.out.println("Конструктор Line()");
     }
 
-    void showId(){
+    void showId() {
         super.showId();
         System.out.println("id = " + id);
     }
